2975b1d2a59f00a1599e957699a89f1430cca8e1
Atharva Wake <wakeatharava@gmail.com>
2025-07-23 23:46:30 +0530

    model trainer completed

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -5,6 +5,9 @@ import os, sys
 import numpy as np
 import dill
 import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score, accuracy_score
 
 def read_yaml_file(file_path: str) -> dict:
     try:
@@ -49,3 +52,56 @@ def save_object(file_path: str, obj: object) -> None:
         logging.info("Exited the save_object method in MainUtils class")
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
+    
+def load_object(file_path: str,) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"the file: {file_path} is not exists")
+        with open(file_path,"rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys) from e 
+    
+def load_numpy_array_data(file_path: str)-> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """      
+    try:
+        with open(file_path,"rb")as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)from e 
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            para = param[model_name]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = f1_score(y_train, y_train_pred, average='binary')
+            test_model_score = f1_score(y_test, y_test_pred, average='binary')
+
+            report[model_name] = {
+                "Train F1 Score": train_model_score,
+                "Test F1 Score": test_model_score,
+                "Best Params": gs.best_params_
+            }
+
+        return report
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
